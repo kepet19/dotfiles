16b0834b004fd6157764ccfa877c3dd2e52fd0c1
kepet19 <kevinmeejach@gmail.com>
2022-02-24 01:30:54 +0100

    RIP functions nnoojnon

--- nvim/.config/nvim/lua/kevz/luasnip.lua
+++ nvim/.config/nvim/lua/kevz/luasnip.lua
@@ -15,24 +15,23 @@ ls.config.set_config {
   },
 }
 
-vim.keymap.set({ "i", "s" }, "<c-k>", function()
-  if ls.expand_or_jumpable() then
-    ls.expand_or_jump()
-  end
-end, { silent = true })
+-- vim.keymap.set({ "i", "s" }, "<c-k>", function()
+--   if ls.expand_or_jumpable() then
+--     ls.expand_or_jump()
+--   end
+-- end, { silent = true })
+
+-- vim.keymap.set({ "i", "s" }, "<c-j>", function()
+--   if ls.jumpable(-1) then
+--     ls.jump(-1)
+--   end
+-- end, { silent = true })
+
+-- vim.keymap.set("i", "<c-l>", function()
+--   if ls.choice_active() then
+--       ls.change_choice(1)
+--   end
+-- end)
 
 
-vim.keymap.set({ "i", "s" }, "<c-j>", function()
-  if ls.jumpable(-1) then
-    ls.jump(-1)
-  end
-end, { silent = true })
-
-vim.keymap.set("i", "<c-l>", function()
-  if ls.choice_active() then
-      ls.change_choice(1)
-  end
-end)
-
-
-vim.keymap.set("n", "<leader><leader>s", "<cmd>source ~/.config/nvim/after/plugin/luasnip.lua<CR>")
+-- vim.keymap.set("n", "<leader><leader>s", "<cmd>source ~/.config/nvim/after/plugin/luasnip.lua<CR>")
