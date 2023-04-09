587568494476372d6d9f17d7237e4e9fae3c6bb3
Kevin Kamper Meejach Petersen <kkmp@netcompany.com>
2023-04-09 12:25:39 +0200

    Add deploy dotfiles for windows

---
+++ deploy_dotfiles_windows.ps1
@@ -0,0 +1,34 @@
+$List = @(
+[pscustomobject]@{name = "nushell"; isLocal = $false},
+[pscustomobject]@{name = "nvim"; isLocal = $true}
+)
+
+foreach ($item in $list) {
+        $name = $item.name
+        $source = Resolve-Path("./*/*/${name}/")
+        $dest = $item.isLocal ? $env:LOCALAPPDATA : $env:APPDATA
+        $dest = $(Join-Path -Path $dest -ChildPath $name)
+        $dest
+        $isValidPath = Test-Path $dest
+        if ($isValidPath) {
+            Write "Can't create junction ${name}"
+            Get-Item $dest
+        } else {
+            Write "Creating junction link for ${name}"
+            New-Item -ItemType Junction -Value $source -Path $dest
+        }
+}
+
+    function Set-Dotfiles(
+            [parameter(Mandatory=$true)] [string]$SourcePath, 
+            [parameter(Mandatory=$true)] [string]$DestinationPath, 
+            [switch]$Force
+            )
+{
+    Get-Location
+        $test = Resolve-Path($SourcePath)
+        $test2= Resolve-Path($DestinationPath)
+        $test
+        $test2
+        $Force
+}
