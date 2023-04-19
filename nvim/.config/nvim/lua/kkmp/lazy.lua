6f205154d5bdd4ab8fb43c9791b13a401fa6c309
Kevin Kamper Meejach Petersen <kkmp@netcompany.com>
2023-04-19 12:05:55 +0200

    Add: null-ls lsp

--- nvim/.config/nvim/lua/kkmp/lazy.lua
+++ nvim/.config/nvim/lua/kkmp/lazy.lua
@@ -1,66 +1,82 @@
 -- Boostrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	-- Color schemes
-	"folke/tokyonight.nvim",
-    {"folke/todo-comments.nvim",
-    config = function()
-        require("todo-comments").setup({
-        }
-
-        )
-    end
-},
-	"rose-pine/neovim",
-	-- Color scheme helpers
-	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+    -- Color schemes
+    "folke/tokyonight.nvim",
+    {
+        "folke/todo-comments.nvim",
+        config = function()
+            require("todo-comments").setup({
+            })
+        end
+    },
+    "rose-pine/neovim",
+    -- Color scheme helpers
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
     "MTDL9/vim-log-highlighting",
 
-	-- functionally
-	{
-		"nvim-telescope/telescope.nvim", tag = "0.1.1",
-		dependencies = { "nvim-lua/plenary.nvim" }
-	},
-	"theprimeagen/harpoon",
-	"mbbill/undotree",
+    -- functionally
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.1",
+        dependencies = { "nvim-lua/plenary.nvim" }
+    },
+    "theprimeagen/harpoon",
+    "mbbill/undotree",
     "tpope/vim-commentary",
-	"tpope/vim-fugitive",
-    {"lewis6991/gitsigns.nvim",
+    "tpope/vim-fugitive",
+    {
+        "lewis6991/gitsigns.nvim",
         config = function()
             require('gitsigns').setup()
         end
     },
 
-	{
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v2.x',
-		dependencies = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},             -- Required
-			{                                      -- Optional
-			'williamboman/mason.nvim',
-			build = function()
-				pcall(vim.cmd, 'MasonUpdate')
-			end,
-		},
-		{'williamboman/mason-lspconfig.nvim'}, -- Optional
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        dependencies = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' }, -- Required
+            {
+                              -- Optional
+                'williamboman/mason.nvim',
+                build = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+            {
+                'jose-elias-alvarez/null-ls.nvim',
+                config = function()
+                    local null_ls = require("null-ls")
+                    null_ls.setup({
+                        sources = {
+                            -- null_ls.builtins.diagnostics.cspell, null_ls.builtins.code_actions.cspell
+                            null_ls.builtins.formatting.xmllint.with({
+                                timeout = 10000
+                            })
+                        }
+                    })
+                end
+            },
 
-		-- Autocompletion
-		{'hrsh7th/nvim-cmp'},     -- Required
-		{'hrsh7th/cmp-nvim-lsp'}, -- Required
-		{'L3MON4D3/LuaSnip'},     -- Required
-	}
-}
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' }, -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'L3MON4D3/LuaSnip' }, -- Required
+        }
+    }
 }, opts)
