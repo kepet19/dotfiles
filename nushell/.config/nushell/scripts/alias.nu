4f52cf8adafe753d088e95f652ab75a8c2383313
Kevin Kamper Meejach Petersen <kkmp@netcompany.com>
2023-05-05 18:26:51 +0200

    Add highlight group when copying

--- nushell/.config/nushell/scripts/alias.nu
+++ nushell/.config/nushell/scripts/alias.nu
@@ -3,4 +3,5 @@ alias la = ls -l -a
 alias edit = config_edit
 
 alias mvcd = cd ~/dev/ad-portal/
+alias main = cd ~/dev/main-portal/
 alias fire = cd ~/dev/firewall-portal/
--- nvim/.config/nvim/lua/kkmp/init.lua
+++ nvim/.config/nvim/lua/kkmp/init.lua
@@ -2,3 +2,36 @@ require("kkmp.global")
 require("kkmp.set")
 require("kkmp.remap")
 require("kkmp.lazy")
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+local yank_group = augroup('HighlightYank', {})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+local RemoveWhiteSpace = augroup('ThePrimeagen', {})
+
+autocmd({"BufWritePre"}, {
+    group = RemoveWhiteSpace,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
